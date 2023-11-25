7f28bc5375cd3df88de5b6bf4dd3ec25e7b42d6e
Yarisingu तरुण <137394306+yarasingu@users.noreply.github.com>
2023-11-25 14:18:11 +0000

    db connection done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # kivy-web
+
+
+# folder structure 
+kivy-web/
+|-- LICENSE
+|-- README.md
+|-- app.py
+|-- requirements.txt
+|-- venv/
+|   |-- (virtual environment files)
+|-- templates/
+|   |-- login.html
--- app.py
+++ app.py
@@ -1,10 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:2F3c5ABd451df1eg*1cfc4g6eGDa23CA@roundhouse.proxy.rlwy.net:31921/railway"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a random secret key
 
-@app.route('/')
-def hello_world():
-    return 'Hello tarun'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Define User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return f'Hello, {current_user.username}! This is your dashboard.'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
